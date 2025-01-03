85e60ce3fa6bc8a8cdfa04d25926d94539ea02c3
Justnibio <36718015+justnibio@users.noreply.github.com>
2025-01-03 16:56:26 +0100

    Update rpcs3-appImage.sh
    rp_module_flags changed
    wget link changed

--- rpcs3-appImage.sh
+++ rpcs3-appImage.sh
@@ -14,11 +14,11 @@ rp_module_desc="PS3 emulator RPCS3"
 rp_module_help="ROM Extensions: .ps3\n\nCopy your .PS3 game folders to $romdir/ps3\n\nDon't forget to run system firmware update first!"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/RPCS3/rpcs3/master/LICENSE"
 rp_module_section="exp"
-rp_module_flags="!arm"
+rp_module_flags="!all x86"
 
 function install_bin_rpcs3-appImage() {
     mkdir -p "$md_inst/bin"
-    wget --content-disposition https://rpcs3.net/latest-appimage -O "$md_inst/bin/rpcs3.AppImage"
+    wget https://github.com/RPCS3/rpcs3-binaries-linux-arm64/releases/download/build-ff0249d662fcedee602c9206e73b5feb91f164c1/rpcs3-v0.0.34-17222-ff0249d6_linux_aarch64.AppImage -O "$md_inst/bin/rpcs3.AppImage" -O "$md_inst/bin/rpcs3.AppImage"
     chmod +x "$md_inst/bin/rpcs3.AppImage"
 }
 
