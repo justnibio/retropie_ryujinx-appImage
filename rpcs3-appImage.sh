16b98b4283d207cdae554e51ea670fb1768246e8
Justnibio <36718015+justnibio@users.noreply.github.com>
2025-01-05 18:58:00 +0100

    Update rpcs3-appImage.sh

--- rpcs3-appImage.sh
+++ rpcs3-appImage.sh
@@ -1,31 +1,59 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
-#
-# The RetroPie Project is the legal property of its developers, whose names are
-# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
-#
-# See the LICENSE.md file at the top-level directory of this distribution and
-# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
-#
-
 rp_module_id="rpcs3-appImage"
 rp_module_desc="PS3 emulator RPCS3"
 rp_module_help="ROM Extensions: .ps3\n\nCopy your .PS3 game folders to $romdir/ps3\n\nDon't forget to run system firmware update first!"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/RPCS3/rpcs3/master/LICENSE"
 rp_module_section="exp"
-rp_module_flags="!all 64bit"
+rp_module_flags=""
+rp_module_repo="file https://api.github.com/repos/RPCS3/rpcs3-binaries-linux-arm64/releases/latest"
+
+function download_latest_rpcs3() {
+    echo "Inizio il download dell'ultima versione di RPCS3 per ARM64..."
+
+    # URL dell'API GitHub per la release
+    api_url="https://api.github.com/repos/RPCS3/rpcs3-binaries-linux-arm64/releases/latest"
+
+    echo "Recupero del link al file AppImage tramite API..."
+    appimage_url=$(curl -s "$api_url" | grep -oP '(?<="browser_download_url": ")[^"]*\.AppImage')
+
+    if [[ -z $appimage_url ]]; then
+        echo "Errore: non    stato trovato alcun file AppImage tramite API."
+        return 1
+    fi
+
+    echo "URL del file AppImage: $appimage_url"
+
+    echo "Download del file AppImage in corso..."
+    wget --content-disposition "$appimage_url" -O rpcs3.AppImage
+
+    if [[ $? -ne 0 ]]; then
+        echo "Errore durante il download del file AppImage."
+        return 1
+    fi
+
+    echo "Download completato: rpcs3.AppImage"
+}
+
 
 function install_bin_rpcs3-appImage() {
+    echo "Inizio installazione RPCS3"
     mkdir -p "$md_inst/bin"
-    wget https://github.com/RPCS3/rpcs3-binaries-linux-arm64/releases/download/build-ff0249d662fcedee602c9206e73b5feb91f164c1/rpcs3-v0.0.34-17222-ff0249d6_linux_aarch64.AppImage -O "$md_inst/bin/rpcs3.AppImage"
+
+    # Chiamata alla funzione principale
+    download_latest_rpcs3
+
     chmod +x "$md_inst/bin/rpcs3.AppImage"
+    echo "Installazione completata"
 }
 
 function configure_rpcs3-appImage() {
+    echo "Inizio configurazione RPCS3"
     mkRomDir "ps3"
     addEmulator 0 "$md_id" "ps3" "$md_inst/bin/rpcs3.AppImage %ROM%/PS3_GAME/USRDIR/EBOOT.BIN"
     addEmulator 1 "$md_id-nogui" "ps3" "$md_inst/bin/rpcs3.AppImage --no-gui %ROM%/PS3_GAME/USRDIR/EBOOT.BIN"
     addEmulator 2 "$md_id-guionly" "ps3" "$md_inst/bin/rpcs3.AppImage"
     addSystem "ps3"
+    echo "Configurazione completata"
 }
+
